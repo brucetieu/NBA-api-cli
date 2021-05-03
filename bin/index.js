1ff46e9c4049309d5dbd48c690f69daaea2211b2
brucetieu <brucetieu5@gmail.com>
2021-05-03 04:51:51 -0600

    Add initial files

---
+++ bin/index.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const api = require('../lib/api')
+
+program
+  .version('0.0.1')
+  .option('-lp, --list-players', 'List all NBA Players', api.getPlayers)
+  .option('-n, --number', 'Get total number of NBA players', api.getNumberOfPlayers)
+
+// must be before .parse() since
+// node's emit() is immediate
+
+program.on('--help', function(){
+  console.log('  Examples:');
+  console.log('');
+  console.log('    $ custom-help --help');
+  console.log('    $ custom-help -h');
+  console.log('');
+});
+
+program.parse(process.argv);
+
