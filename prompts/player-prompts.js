42c0f471e4390f00d9479f34e948b1168740a25b
brucetieu <brucetieu5@gmail.com>
2021-05-05 06:17:20 -0600

    Return prompt directly instead of storing it in var

--- prompts/player-prompts.js
+++ prompts/player-prompts.js
@@ -1,7 +1,7 @@
 const inquirer = require("inquirer");
 
-const playerOptionPrompt = async () => {
-  const prompt = inquirer.prompt([
+const playerOptionPrompt = () => {
+  return inquirer.prompt([
     {
       type: "list",
       name: "options",
@@ -9,40 +9,36 @@ const playerOptionPrompt = async () => {
       choices: ["per_page (default = 25)", "page", "player_search"],
     },
   ]);
-  return prompt;
 };
 
-const perPagePrompt = async () => {
-  const prompt = inquirer.prompt([
+const perPagePrompt = () => {
+  return inquirer.prompt([
     {
       type: "input",
       name: "perPageInput",
       message: "Enter the number of players you want to see per page",
     },
   ]);
-  return prompt;
 };
 
-const pagePrompt = async () => {
-  const prompt = inquirer.prompt([
+const pagePrompt = () => {
+  return inquirer.prompt([
     {
       type: "input",
       name: "pageInput",
       message: "Enter the page number for viewing players",
     },
   ]);
-  return prompt;
 };
 
-const playerSearchPrompt = async () => {
-  const prompt = inquirer.prompt([
+const playerSearchPrompt = () => {
+  return inquirer.prompt([
     {
       type: "input",
       name: "playerInput",
       message: "Enter the first or last name of the player",
     },
   ]);
-  return prompt;
 };
 
 module.exports = {
