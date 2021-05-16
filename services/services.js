51d8d75ba4e88f80eab5d59502d9e6fa51ba7a0b
brucetieu <brucetieu5@gmail.com>
2021-05-15 19:00:05 -0600

    Add try/catch

--- services/services.js
+++ services/services.js
@@ -25,20 +25,24 @@ const getFilteredPlayersData = async (nbaApiUrl) => {
 const getSeasonAvgPerPlayer = async (url, output) => {
   let queryString = "";
 
-  for (let i = 0; i < output.length - 1; i++) {
-    queryString += `player_ids[]=${output[i]}&`;
-  }
-  queryString += `season=${output[output.length - 1]}`;
+  try {
+    for (let i = 0; i < output.length - 1; i++) {
+      queryString += `player_ids[]=${output[i]}&`;
+    }
+    queryString += `season=${output[output.length - 1]}`;
 
-  const response = await axios.get(url + "/season_averages?" + queryString);
+    const response = await axios.get(url + "/season_averages?" + queryString);
 
-  for (let resp of response.data.data) {
-    let queriedPlayer = await saveAxiosData(
-      url + `/players/${resp.player_id}`
-    );
-    let full_name =
-      queriedPlayer.data.first_name + " " + queriedPlayer.data.last_name;
-    console.log({ full_name: full_name, ...resp });
+    for (let resp of response.data.data) {
+      let queriedPlayer = await saveAxiosData(
+        url + `/players/${resp.player_id}`
+      );
+      let full_name =
+        queriedPlayer.data.first_name + " " + queriedPlayer.data.last_name;
+      console.log({ full_name: full_name, ...resp });
+    }
+  } catch (err) {
+    console.log({ message: err.message, name: err.name });
   }
 };
 
